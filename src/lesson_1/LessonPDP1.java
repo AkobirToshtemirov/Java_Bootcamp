8fcb52d4ae78fb69316d8b422a172d384a1e2323
Akobir5 <toshtemirovakobir8@gmail.com>
2023-03-15 11:54:51 +0500

    command-line argument arguments

--- src/lesson_1/LessonPDP1.java
+++ src/lesson_1/LessonPDP1.java
@@ -2,6 +2,9 @@ package lesson_1;
 
 public class LessonPDP1 {
     public static void main(String[] args) {
-
+        System.out.println("Start program!");
+        for(int i = 0; i < args.length; i++) {
+            System.out.println("Length of args: " + i);
+        }
     }
 }
