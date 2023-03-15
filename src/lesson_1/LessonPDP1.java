f066c7ae0fdd81a51c78023e50205101a46460b8
Akobir5 <toshtemirovakobir8@gmail.com>
2023-03-15 19:26:19 +0500

    command-line argument arguments check

--- src/lesson_1/LessonPDP1.java
+++ src/lesson_1/LessonPDP1.java
@@ -4,7 +4,7 @@ public class LessonPDP1 {
     public static void main(String[] args) {
         System.out.println("Start program!");
         for(int i = 0; i < args.length; i++) {
-            System.out.println("Length of args: " + i);
+            System.out.println("Length of args: " + args[i]);
         }
     }
 }
