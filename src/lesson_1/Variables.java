f5255533b520ffc8ae43d3a93804cd5cc1e8dae4
Akobir5 <toshtemirovakobir8@gmail.com>
2023-03-16 23:36:06 +0500

    types of variables

---
+++ src/lesson_1/Variables.java
@@ -0,0 +1,24 @@
+package lesson_1;
+
+import com.sun.jdi.LocalVariable;
+
+public class Variables {
+
+    int age =19;
+    String person = "Student";
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+    public void localVariable() {
+        int localNum = 12;
+        String localText = "Text";
+        System.out.println(age);
+        System.out.println(person);
+    }
+
+
+}
