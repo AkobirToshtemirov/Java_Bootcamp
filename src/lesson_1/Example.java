6e8b08823bcbd5d627d4d811ae30dd25e00336ed
Akobir5 <toshtemirovakobir8@gmail.com>
2023-03-17 20:07:22 +0500

    operators: passing the parameters

---
+++ src/lesson_1/Example.java
@@ -0,0 +1,30 @@
+package lesson_1;
+
+import java.util.Arrays;
+
+public class Example {
+    public static void main(String[] args) {
+        int x = 10;
+        int[] arr = {1, 2, 3};
+
+        // Pass by value
+        System.out.println("Before: " + x);
+        incrementValue(x);
+        System.out.println("After: " + x);
+
+        // Pass by reference
+        System.out.println("Before: " + Arrays.toString(arr));
+        modifyArray(arr);
+        System.out.println("After: " + Arrays.toString(arr));
+    }
+
+    // Method that takes a parameter by value
+    public static void incrementValue(int value) {
+        value++;
+    }
+
+    // Method that takes a parameter by reference
+    public static void modifyArray(int[] array) {
+        array[0] = 100;
+    }
+}
