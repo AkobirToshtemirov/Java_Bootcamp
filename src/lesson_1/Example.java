2bf4c09bbed2be44b64a9cfaa8964b509abc2721
Akobir5 <toshtemirovakobir8@gmail.com>
2023-03-22 18:25:36 +0500

    examples of algo

--- src/lesson_1/Example.java
+++ src/lesson_1/Example.java
@@ -4,18 +4,22 @@ import java.util.Arrays;
 
 public class Example {
     public static void main(String[] args) {
-        int x = 10;
-        int[] arr = {1, 2, 3};
+//        int x = 10;
+//        int[] arr = {1, 2, 3};
+//
+//        // Pass by value
+//        System.out.println("Before: " + x);
+//        incrementValue(x);
+//        System.out.println("After: " + x);
+//
+//        // Pass by reference
+//        System.out.println("Before: " + Arrays.toString(arr));
+//        modifyArray(arr);
+//        System.out.println("After: " + Arrays.toString(arr));
 
-        // Pass by value
-        System.out.println("Before: " + x);
-        incrementValue(x);
-        System.out.println("After: " + x);
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        int[] b = new int[]{1, 2, 3, 4, 5, 6, 7, 7, 8, 8};
 
-        // Pass by reference
-        System.out.println("Before: " + Arrays.toString(arr));
-        modifyArray(arr);
-        System.out.println("After: " + Arrays.toString(arr));
     }
 
     // Method that takes a parameter by value
