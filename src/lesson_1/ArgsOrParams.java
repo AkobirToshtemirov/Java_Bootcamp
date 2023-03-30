dbcc55b3d9a25e14d050683a5656a6b70cff31dd
Akobir5 <toshtemirovakobir8@gmail.com>
2023-03-30 07:05:56 +0500

    working with API doc

--- src/lesson_1/ArgsOrParams.java
+++ src/lesson_1/ArgsOrParams.java
@@ -2,9 +2,11 @@ package lesson_1;
 
 public class ArgsOrParams {
     public static void main(String[] args) {
+        int i = 1;
         for (String s :
                 args) {
-            System.out.println(s);
+            System.out.println(i + s);
+            i++;
         }
     }
 }
