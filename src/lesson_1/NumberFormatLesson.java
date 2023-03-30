ea092ccfa18522e0bdcfba25c37af3fc3466cccd
Akobir5 <toshtemirovakobir8@gmail.com>
2023-03-30 15:13:36 +0500

    Formatting Currency in Java

--- src/lesson_1/NumberFormatLesson.java
+++ src/lesson_1/NumberFormatLesson.java
@@ -1,6 +1,7 @@
 package lesson_1;
 
 import java.text.NumberFormat;
+import java.util.Locale;
 
 public class NumberFormatLesson {
     public static void main(String[] args) {
@@ -16,5 +17,28 @@ public class NumberFormatLesson {
 
         NumberFormat intNf = NumberFormat.getIntegerInstance();
         System.out.println(intNf.format(dval));
+
+        System.out.println("==============");
+
+        nf.setGroupingUsed(false); // removes " , " in the num
+        System.out.println(nf.format(dval));
+
+        // For particular language in particular country use " Locale "
+
+        Locale locale = new Locale("de", "DE"); // for Germany
+        NumberFormat localFormat = NumberFormat.getNumberInstance(locale);
+        System.out.println(localFormat.format(dval));
+
+        // Currency Format
+
+        NumberFormat currencyFormat = NumberFormat.getCurrencyInstance();
+        System.out.println(currencyFormat.format(dval));
+
+        System.out.println(" ");
+        Locale localeUz = new Locale("uz", "UZ"); // for Uzbekistan
+        NumberFormat currencyFormatUz = NumberFormat.getCurrencyInstance(localeUz);
+        System.out.println(currencyFormatUz.format(dval));
+
+
     }
 }
